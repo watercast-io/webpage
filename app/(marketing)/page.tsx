ef003e6dd1a13b596b6540a6cfe5e700678282bc
Sergio Esteban <sergio.esteban.ce@gmail.com>
2024-05-01 20:18:01 +0200

    add text-color fix

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -6,7 +6,7 @@ import Link from "next/link"
 export default async function IndexPage() {
   return (
     <div className="">
-      <section className="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32 text-black">
+      <section className="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32">
         <div className="container flex max-w-5xl flex-col items-center gap-4 text-center">
           <h1 className="font-heading text-3xl sm:text-5xl md:text-6xl lg:text-7xl ">
           AI-driven water resilience solution for risk assessment
