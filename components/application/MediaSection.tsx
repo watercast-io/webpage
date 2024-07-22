6fb916e2f53e38d2564ff87084dd2a546100851c
Sergio Esteban <sergio.esteban.ce@gmail.com>
2024-07-22 21:21:31 +0200

    add locales, language changer

--- components/application/MediaSection.tsx
+++ components/application/MediaSection.tsx
@@ -8,22 +8,15 @@ import watercastVideo from '@/videos/watercastVideo.mp4';
 export interface MediaSectionProps {
     children?: React.ReactNode
 }
-
-
-const MediaSection: React.FC<MediaSectionProps> = ({ children }) => {
-
+const MediaSection: React.FC<MediaSectionProps> = ({ }) => {
     return(
         <React.Fragment>
             <section 
                 id="open-source" 
-                //className="container py-8 md:py-12 lg:py-24"
                 className="container space-y-6 py-8 dark:bg-transparent md:py-12 lg:py-24"
             >
                 <div className="mx-auto flex max-w-[58rem] flex-col items-center justify-center gap-4 text-center">
-                    <h2 className="font-heading text-3xl leading-[1.1] sm:text-3xl md:text-6xl">
-                        {/* Book a demo */}
-                    </h2>
-                    <Video src={watercastVideo} />;
+                    <Video src={watercastVideo} />
                 </div>
                 
             </section>
