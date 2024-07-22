3f786efa60a9ac4e110ec728b9b00ec281fbd9ab
Sergio Esteban <sergio.esteban.ce@gmail.com>
2024-07-22 21:42:00 +0200

    add prefixDefault: boolean;

--- i18nConfig.ts
+++ i18nConfig.ts
@@ -1,11 +1,13 @@
 interface I18nConfig {
     locales: string[];
     defaultLocale: string;
+    prefixDefault: boolean;
 }
 
 const i18nConfig: I18nConfig = {
     defaultLocale: "en",
-    locales: ["en", "es"]
+    locales: ["en", "es"],
+    prefixDefault: false,
 };
 
 export default i18nConfig;
